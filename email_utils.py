7ae2db4c777376915cc4f8f763dfed6dd6455289
Hanz Po <hanznathanpo@gmail.com>
2023-05-06 09:54:22 -0400

    Combine email scripts into single file

--- email_utils.py
+++ email_utils.py
@@ -1,6 +1,24 @@
-import os
+from bs4 import BeautifulSoup
+import requests
+import regex as re
 import openai
-openai.api_key = "API_KEY"
+
+openai.api_key = "API KEY"
+
+email = re.compile('[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+[.][A-Za-z.]{2,}')
+
+def get_emails(url):
+
+    req = requests.get(url)
+
+    content = req.text
+
+    soup = BeautifulSoup(content, features="html.parser")
+
+    emails = email.findall(str(soup.find('main')))
+    emails = list(dict.fromkeys(emails))
+
+    return emails
 
 def get_research_template(prof, prof_uni, prof_topic, student_name, student_position, student_uni, student_topic):
     completion = openai.ChatCompletion.create(
@@ -11,5 +29,7 @@ def get_research_template(prof, prof_uni, prof_topic, student_name, student_posi
     )
 
     return completion.choices[0].message.content
+
+print(get_emails(input("Please enter a url -> ")))
 
 print(get_research_template("Ashton Anderson", "University of Toronto", "Artificial intelligence", "Robert Jordan", "first year student", "University of Waterloo", "Image classification"))
