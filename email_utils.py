9fd1e04c647f0c049e710d7a5cfe4d9c32d68a6c
Hanz Po <hanznathanpo@gmail.com>
2023-05-06 10:38:35 -0400

    Add internship email generator

--- email_utils.py
+++ email_utils.py
@@ -3,7 +3,7 @@ import requests
 import regex as re
 import cohere
 
-co = cohere.Client('')
+co = cohere.Client('GAnzo3F1hA7HnixKfwRPiMarojJC9Kwe8Wwi0bVk')
 
 email = re.compile('[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+[.][A-Za-z.]{2,}')
 name = re.compile('^[\w\'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$')
@@ -23,12 +23,25 @@ def get_emails(url):
 
 
 def get_research_template(prof, prof_uni, prof_topic, student_name, student_position, student_uni, student_topic):
+    prompt = f"Write a cold outreach email to a professor named {prof} at {prof_uni} who is currently researching {prof_topic} from a student named {student_name}, who is a {student_position} at {student_uni}, asking if {prof} is interested in hiring {student_name} as a research assistant regarding {student_topic}. Do not generate emails or phone numbers. Only ask if they are open to hiring people"
     response = co.generate(
   model='command-xlarge-nightly',
-  prompt=f"Write a cold outreach email to a professor named {prof} at {prof_uni} who is currently researching {prof_topic} from a student named {student_name}, who is a {student_position} at {student_uni}, asking if {prof} is interested in hiring {student_name} as a research assistant regarding {student_topic}. Do not generate emails or phone numbers. Only ask if they are open to hiring people"
-,
+  prompt=prompt,
   max_tokens=300,
-  temperature=0.9,
+  temperature=0.5,
+  k=0,
+  stop_sequences=[],
+  return_likelihoods='NONE')
+
+    return response.generations[0].text
+
+def get_internship_template(company_employee, company_name, student_name, student_position, student_uni):
+    prompt = f"Write a cold outreach email to a person named {company_employee} at {company_name} from a student named {student_name}, who is a {student_position} at {student_uni}, asking if {company_name} is interested in hiring {student_name} as an intern. Do not generate emails or phone numbers. Only ask if they are open to hiring people"
+    response = co.generate(
+  model='command-xlarge-nightly',
+  prompt=prompt,
+  max_tokens=300,
+  temperature=0.5,
   k=0,
   stop_sequences=[],
   return_likelihoods='NONE')
@@ -47,6 +60,8 @@ def get_names(url):
 
     return names
 
-# print(get_names(input("Please enter a url -> ")))
+# TESTING
 
-print(get_research_template("Alice Smith", "Toronto Metropolitan University", "artificial intelligence", "Bob Ross", "first year student", "Toronto Metropolitan University", "natural language processing"))
+# print(get_names(input("Please enter a url -> ")))
+# print(get_research_template("Alice Smith", "Toronto Metropolitan University", "artificial intelligence", "Bob Ross", "first year student", "Toronto Metropolitan University", "natural language processing"))
+# print(get_internship_template("Alice Smith", "Cohere", "Bob Ross", "first year Computer Science student", "Toronto Metropolitan University"))
